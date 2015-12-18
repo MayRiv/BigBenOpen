e67322a75ae9e849d57883effebcecc1ff55a9f1
mayriv <mayriv@i.ua>
2015-12-18 18:50:39 +0200

    Changed dbname and username in placing, also, added some checking.

--- placing.php
+++ placing.php
@@ -1,8 +1,8 @@
 <?php
 require("DBManager.inc");
 $host = "mysql.hostinger.com.ua";
-$dbName = "u907137641_open";
-$user = "u907137641_open";
+$dbName = "u825515718_open";
+$user = "u825515718_open";
 $password = "konoplya_1";
 DBManager::getInstance()->connect($host, $dbName, $user, $password);
 class Player 
@@ -16,6 +16,29 @@ class Player
 	public function setTable($talbeNumber)
 	{
 		$this->table[] = $talbeNumber; 
+	}
+}
+class Table
+{
+	public $id;
+	public $rounds;	
+	function __construct($id)
+	{
+		$this->id = $id;
+	}
+	public function setPlayer($round, $player)
+	{
+		if (!in_array($player, $this->rounds[$round]))
+			$this->rounds[$round][] = $player; 
+	}
+	public checkTable()
+	{
+		for ($i = 0; $i < 7; $i++)
+		{
+			if (count($this->rounds[$i]) != 10)
+				return 0;
+		}
+		return 1;
 	}
 }
 $players = array();
@@ -32,7 +55,28 @@ for ($i=1; $i < 7; $i++) {
 		}
 	}
 }
-
+$tables = array();
+for ($i = 0; $i < 10; $i++)
+{
+	$tables[] = new Table($i);
+}
+for ($i = 0; $i < 100; $i++)
+{
+	for($j = 0; $j < 7; $j++)
+	{
+		$tables[$players[$i]->table[$j]]->setPlayer($j,$players[$i]->id);
+	}
+}
+$result = true;
+for ($i = 0; $i < 10; $i++)
+{
+	if (!$tables[$i]->check())
+		$result = false;
+}
+if ($result)
+	echo "Success.";
+else 
+	echo "Failure";
 
 for ($i=0; $i < 100; $i++) { 
 	$tables = "";
